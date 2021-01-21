6d51c496b9eb86c14eb42e747030fc4578e74048
wmt36 <wtutein@gmail.com>
2021-01-21 04:11:07 -0800

    Added custom slider component

--- src/App.vue
+++ src/App.vue
@@ -11,18 +11,11 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin-top: 10vh;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+body {
+  margin: 0;
+  background-color: black;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Slider />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Slider from '@/components/Slider.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Slider
+  },
+  methods: {
+    enlarge(event) {
+      event.currentTarget.classList.add('large');
+    }
   }
 }
 </script>
