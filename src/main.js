89bcf0a6795f3f6539e4d7db772e764b94b44b33
wmt36 <wtutein@gmail.com>
2021-01-21 01:09:21 -0800

    Added vuetify

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import vuetify from '@/plugins/vuetify'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
@@ -7,6 +8,7 @@ import store from './store'
 Vue.config.productionTip = false
 
 new Vue({
+  vuetify,
   router,
   store,
   render: h => h(App)
